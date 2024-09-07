d50a58a0203786b8861eb715c8e180a12913efba
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-07 17:52:14 +0800

    mithorium-common: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.19) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- mithorium.mk
+++ mithorium.mk
@@ -27,6 +27,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
